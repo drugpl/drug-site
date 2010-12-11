ccac772c8b4956c57bf47a83ab1010d80eee1f46
Paweł Pacana <pawel.pacana@gmail.com>
2010-12-11 15:02:46 +0100

    initial layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  PAGES = %w(home)
+
+  def site_menu
+    PAGES.collect { |page| [I18n.t("site_menu.pages.#{page}"), page == 'home' ? root_path : send("#{page}_path")] }
+  end
 end

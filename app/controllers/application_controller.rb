c01f092e8feeb0c5b85268e3e8ea1b77ef8e0a3e
Paweł Pacana <pawel.pacana@gmail.com>
2010-12-11 23:48:10 +0100

    hack for custom locale in admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+
+  protected
+
+  def set_locale
+    I18n.locale = case self.class.name
+      when /RailsAdmin/ then :en
+      else I18n.default_locale
+    end
+  end
 end

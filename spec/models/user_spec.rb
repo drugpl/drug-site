d3af9b57fcd9c32f78ab9b83431355e8f29fa870
Paweł Pacana <pawel.pacana@gmail.com>
2010-12-12 02:48:02 +0100

    user tests and associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = Factory.build(:user)
+  end
+
+  it "should have valid factories" do
+    @user.should be_valid
+  end
+  
+  it "should require email" do
+    @user.email = nil
+    @user.save
+    @user.should have(1).error_on(:email)
+  end
+
+  it "should require password" do
+    @user.password = nil
+    @user.save
+    @user.should have(1).error_on(:password)
+  end
 end

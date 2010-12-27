3b98dacd25790142d35555780ff61d68225b7479
Paweł Pacana <pawel.pacana@gmail.com>
2010-12-27 01:47:10 +0100

    removed locale hack to welcome pl translation in rails-admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter :set_locale
-
-  protected
-
-  def set_locale
-    I18n.locale = case self.class.name
-      when /RailsAdmin/ then :en
-      else I18n.default_locale
-    end
-  end
 end

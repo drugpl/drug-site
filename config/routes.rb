96ef4a38d609f6547276e6354135451ac255346f
Paweł Pacana <pawel.pacana@gmail.com>
2010-12-30 22:48:48 +0100

    fixed language init in localized block

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Drug::Application.routes.draw do
   root :to => "home#index"
   devise_for :users
   
-  localized do
+  localized('pl') do
     resources :news_articles, :only => [:index, :show]
     resources :events, :only => [:index, :show]
     resource :contact, :only => [:show, :create]
--- spec/acceptance/homepage_spec.rb
+++ spec/acceptance/homepage_spec.rb
@@ -73,8 +73,8 @@ feature "Homepage" do
 
   scenario "should provide auto discovery links to feeds" do
     @user.visit(homepage)
-    @user.should_discover_rss("http://www.example.com/news_articles.rss")
-    @user.should_discover_rss("http://www.example.com/events.rss")
+    @user.should_discover_rss("http://www.example.com/aktualnosci.rss")
+    @user.should_discover_rss("http://www.example.com/spotkania.rss")
   end
 
   # XXX: problem with selenium not seeing db record

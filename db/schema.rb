04361eec2490c3f19f02268df6592427444bf7a0
Paweł Pacana <pawel.pacana@gmail.com>
2011-01-18 13:37:12 +0100

    active_record store for sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101229155715) do
+ActiveRecord::Schema.define(:version => 20110118035649) do
 
   create_table "contacts", :force => true do |t|
     t.string   "name",       :null => false
@@ -52,6 +52,16 @@ ActiveRecord::Schema.define(:version => 20101229155715) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "slugs", :force => true do |t|
     t.string   "name"

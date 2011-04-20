99f2dd4825374b94374e8d2445c8a3d7235987b5
Jan Dudek <jd@jandudek.com>
2011-04-20 20:29:33 +0200

    added "more" link to twitter feed block

--- app/cells/content_blocks_cell.rb
+++ app/cells/content_blocks_cell.rb
@@ -12,6 +12,7 @@ class ContentBlocksCell < Cell::Rails
   def twitter_feed
     @user = AppConfig[:twitter_user]
     @list = AppConfig[:twitter_list]
+    @url  = AppConfig[:twitter_url]
     render
   end
 end

2ae434bca8539f1b343161dddeac187abf8088be
Jan Dudek <jd@jandudek.com>
2011-04-21 19:43:20 +0200

    validate presence of event description, so events don't break the home page

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,6 +9,7 @@ class Event < ActiveRecord::Base
   has_friendly_id :title, :use_slug => true, :approximate_ascii => true
   
   validates :title, :presence => true
+  validates :description, :presence => true
   validates :starting_at, :presence => true
   validates :user, :presence => true
   validates :venue, :presence => true

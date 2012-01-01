8a06b2930ecb211f93bbb07cddcc050c4cbf21ea
Paweł Pacana <pawel.pacana@gmail.com>
2012-01-01 20:52:38 +0100

    jquery uprade

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,6 @@ module Drug
 
     # Enable the asset pipeline
     config.assets.enabled = false
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 

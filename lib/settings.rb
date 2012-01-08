5f77060713e6d72529bb0e8f30da1078d56d7a53
Paweł Pacana <pawel.pacana@gmail.com>
2012-01-08 21:53:40 +0100

    missing email settings extractor

--- lib/settings.rb
+++ lib/settings.rb
@@ -8,6 +8,10 @@ class Settings
       read_config('config/google_maps.yml')
     end
 
+    def email
+      read_config('config/email.yml')
+    end
+
     private
     def read_config(path)
       YAML.load(Rails.root.join(path).read)[Rails.env].symbolize_keys

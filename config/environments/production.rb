c87246b0535cea5631e8afe61a90fab37f25d32c
Paweł Pacana <pawel.pacana@gmail.com>
2012-03-21 10:50:39 +0100

    added default url to mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,8 @@ Drug::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method    = :smtp
-  config.action_mailer.smtp_settings = Settings.email[:smtp_settings]
+  config.action_mailer.default_url_options = { :host => "drug.org.pl" }
+  config.action_mailer.perform_deliveries  = true
+  config.action_mailer.delivery_method     = :smtp
+  config.action_mailer.smtp_settings       = Settings.email[:smtp_settings]
 end

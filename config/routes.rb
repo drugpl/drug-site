55c0aac2138b3775ef496c161e4744eee5befe9b
Robert Pankowecki <robert.pankowecki@gmail.com>
2012-10-04 23:12:55 +0200

    Remove PL routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Drug::Application.routes.draw do
   root to: "home#index"
+  resources :users, only: [:index, :show]
 
   resources :events, only: [:index, :show] do
     resources :attendants, only: [:index]
   end
   resource :contact, only: [:show, :create]
-
-  # translated routes in PL
-  scope do
-    resources :events, only: [:index, :show], path: 'spotkania' do
-      resources :attendants, only: [:index], path: 'uczestnicy'
-    end
-    resource :contact, only: [:show, :create], path: 'kontakt'
-    resources :presentations, path: 'prezentacje'
-  end
 end

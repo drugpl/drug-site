8676f9cab58d7a8332de9f98a0a149541cd4d6ae
Robert Pankowecki <robert.pankowecki@gmail.com>
2012-10-04 22:14:44 +0200

    Skip redundant requires, use new FG syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
-require 'factory_girl'
-require './spec/factories/presentations'
-require './spec/factories/events'
-require './spec/factories/users'
-require './spec/factories/venues'
+require 'factory_girl_rails'
 
 10.times {
-  Factory(:presentation)
-  Factory(:event)
+  FactoryGirl.create(:presentation)
+  FactoryGirl.create(:event)
 }

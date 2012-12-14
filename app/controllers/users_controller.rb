8be2ac50766a864ae96abc3062ee49d968f599c5
Rafał Łasocha <me+dev@swistak35.com>
2012-12-14 23:35:19 +0100

    don't allow to show user, which has publicized = false (default)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
+  class UnpublicizedUserException < StandardError; end
+
   def index
     @users = User.publicized.sort_by {|u| u.karma }
   end
 
   def show
     @user = User.find(params[:id])
+    raise UnpublicizedUserException unless @user.publicized?
+  rescue UnpublicizedUserException
+    redirect_to users_path, notice: "You are not allowed to see this profile."
   end
 
   def edit

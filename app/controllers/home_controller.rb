f82d7341cae94d9d2faaafc8fabdc06b512649c6
Rafał Łasocha <me+dev@swistak35.com>
2012-12-16 20:55:56 +0100

    moved events partials from views/home to views/events, added /events list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @event = Event.last
-    @previous_events = Event.previous
+    @event = Event.recent.first
     @presentations = @event.presentations.not_postponed
     @postponed_presentations = @event.presentations.postponed
     @participants = @event.users
     @new_presentation = Presentation.new
+    render 'events/show'
   end
 end

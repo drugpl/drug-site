e1714b13994dbaacff5f9307cf9beb88b79a1e3f
Paweł Pacana <pawel.pacana@gmail.com>
2012-12-30 18:57:51 +0100

    Fix events sketch.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,5 +7,8 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    @submitted_presentations = @event.presentations.submitted
+    @postponed_presentations = @event.presentations.postponed
+    @participants = @event.participants
   end
 end

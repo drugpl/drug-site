2346f5b410edd732e62f41b10e572e7d2c5302d9
Paweł Pacana <pawel.pacana@gmail.com>
2013-01-07 02:07:34 +0100

    Duh, analytics are missing.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require google-maps
+//= require google-analytics
 //= require disqus-comments
 //= require disqus-comments-count
 //= require facebook-events

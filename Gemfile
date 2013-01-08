fd5e59918647d317121cb3d9f6101d806b8afbd1
Rafał Łasocha <me+dev@swistak35.com>
2013-01-08 13:01:46 +0100

    added whenever gem; added drug_site method to add new event after previous is gone

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'omniauth-github'
 gem 'localeapp'
 gem 'bbq', :github => 'drugpl/bbq'
 
+gem 'whenever', require: false
+
 group :assets do
   gem 'sass-rails', '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
--- app/models/drug_site.rb
+++ app/models/drug_site.rb
@@ -26,4 +26,35 @@ class DrugSite
     new_account.save!
     previous_account.destroy
   end
+
+  def keep_upcoming_event_alive
+    if Event.upcoming.blank?
+      last_happened = Event.happened.last
+
+      event = Event.new
+      event.venue = last_happened.venue
+      event.description = last_happened.description
+      event.starting_at = third_monday_of_next_month(last_happened.starting_at)
+
+      event.title = "DRUG #"
+      event.title += (last_happened.title.scan(/(\w+)/).last.first.to_i + 1).to_s
+
+      event.save!
+
+      last_happened.postponed_presentations.each do |postponed_presentation|
+        new_presentation = postponed_presentation.dup
+        new_presentation.event = event
+        new_presentation.status = 'submitted'
+        new_presentation.save!
+      end
+    end
+  end
+
+private
+  def third_monday_of_next_month(date)
+    new_date = DateTime.new(date.year, date.month, 1, date.hour)
+    new_date += 1.month
+    new_date += 1 until new_date.monday?
+    new_date += 2.weeks
+  end
 end
